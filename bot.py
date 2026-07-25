f0a5277583e0923b2323b0bb4ae64a05bfd7d8d5
aura961 <a7b2c3d7@icloud.com>
2026-07-26 06:59:14 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,17 +17,18 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = user.id
 
     text = f"""
-🎉 새로운 사용자가 입장했습니다.
+어서오세요 기다리고 있었어요.
+저는 아우라의 문지기, 진실의 입이에요.
+거짓말을 하면 당신의 손을 물어버릴거에요.
 
-👤 닉네임 : {name}
-🔗 사용자명 : @{username if username else "없음"}
-🆔 고유번호 : {user_id}
+ 사용자명 : @{username if username else "없음"}
+ 고유번호 : {user_id}
 """
 
     keyboard = [
         [
             InlineKeyboardButton("제휴문의", url=CONTACT),
-            InlineKeyboardButton("공지채널", url=CHANNEL)
+            InlineKeyboardButton("제3구역", url=CHANNEL)
         ]
     ]
 
